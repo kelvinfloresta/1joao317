ffe66ba1d819c9388f06d4b958a068127de5c6db
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2024-01-16 22:25:57 -0300

    feat(home): improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <div className="min-h-screen flex mt-12 max-w-lg mx-auto p-4">
     <div className="flex flex-col">
-      <h1 className="text-3xl font-bold">{t.title}</h1>
+      <h1 className="text-3xl font-bold text-center">{t.title}</h1>
       <p className="mt-4">{t.subtitle}</p>
       <p className="mt-4">{t.subtitle_2}</p>
 
